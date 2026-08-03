aa5c8b781b0fd6c4a3b97254cdcd94acca3f9eb9
Yash Tailor <tailory123@gmail.com>
2026-08-03 19:51:33 +0530

    03-08-2026

---
+++ Classwork/032_Django_JWT_Auth/myproject/myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,APIView,permission_classes
+from rest_framework.permissions import IsAdminUser,IsAuthenticated,AllowAny
+
+# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    return Response("user api calling...")
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_admin(request):
+    return Response("admin api calling...")
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_normal(request):
+    return Response("normal api calling...")
